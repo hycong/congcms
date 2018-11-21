5bcf1acb0b1e73624644c0ebb34f5c9457df42a0
hyc <627005379@qq.com>
2018-11-21 22:11:58 +0800

    添加入口文件

--- config/template.php
+++ config/template.php
@@ -32,4 +32,11 @@ return [
     'taglib_begin' => '{',
     // 标签库标签结束标记
     'taglib_end'   => '}',
+
+
+    'tpl_replace_string'  =>  [
+        '__PUBLIC__'=>'./static',
+        '__MANAGE__' => '/static/manage',
+    ]
+
 ];
